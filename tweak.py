14d4470f13c60935d02be0a137b2cd8e66056cd7
mohitsingh0298 <mohitsingh0298@gmail.com>
2020-06-06 15:47:35 +0530

    tweak_fix

--- tweak.py
+++ tweak.py
@@ -6,12 +6,12 @@
 
 line_index = 76
 lines = None
-with open('CNN_cat_and_dog.py', 'r') as file_handler:
+with open('/root/task3.py', 'r') as file_handler:
     lines = file_handler.readlines()
 
 lines.insert(line_index, 'CRP(8)\n')
 
-with open('CNN_cat_and_dog.py', 'w') as file_handler:
+with open('/root/task3.py', 'w') as file_handler:
     file_handler.writelines(lines)
 
 
